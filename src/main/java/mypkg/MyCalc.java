126daa5e3556787454acf325b51ff5097632a729
unknown <Administrator@HITESHKUMAR_LTP.DDN.UPES.AC.IN>
2023-09-08 10:15:34 +0530

    First version

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,31 @@
+package mypkg;
+import java.util.Scanner;
+public class MyCalc { 
+	
+	public int sum(int a, int b)
+	{
+		return (a+b);
+	}
+	public int diff(int a, int b)
+	{
+		return (a-b);
+	}
+	public int mul(int a, int b)
+	{
+		return (a*b);
+	}
+	public static void main(String[] args) {
+		
+		MyCalc calc = new MyCalc();
+		Scanner sc = new Scanner(System.in);
+		int num1;
+		int num2;
+		System.out.println("Enter the value of Num1 : ");
+		num1 = sc.nextInt();
+		System.out.println("Enter the value of Num2 : ");
+		num2= sc.nextInt();
+		System.out.println("Sum is : "+calc.sum(num1, num2));
+		System.out.println("Diff is : "+calc.diff(num1, num2));
+		System.out.println("Mul is : "+calc.mul(num1, num2));
+	}
+}
